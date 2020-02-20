a89b99ba20e75ad8299dcc9eb4ba2c090ff890eb
gitsachin <qasachin03@gmail.com>
2020-02-20 18:03:47 +0530

    updated methods

--- src/test/java/Pages/InputPages/TestCore.java
+++ src/test/java/Pages/InputPages/TestCore.java
@@ -16,6 +16,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 
 
@@ -30,21 +31,22 @@ public class TestCore extends Page{
 	public static final String SCREENSHOT_FORMAT = ".png";
 
 	
-	@BeforeMethod(alwaysRun = true)
-	public void setUp(Method method) throws IOException, InterruptedException, MalformedURLException {
-
-		log("--------------------------------------------------------", ILogLevel.TESTCASE);
-		log("---------Test ["+method.getName()+"] Started------------", ILogLevel.TESTCASE);
-		log("--------------------------------------------------------", ILogLevel.TESTCASE);
-
+	@BeforeMethod
+	public void beforMethod(Method method) throws MalformedURLException{
+		
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName", "android");//note 3- 3204a822185f2173// s3-4df1b3c606d75f11
-		//capabilities.setCapability("platformVersion", "5.1.1");
+		capabilities.setCapability("deviceName", "android");//note 3- 3204a822185f2173// s3-4df1b3c606d75f11//android//ZY3223FG76
 		capabilities.setCapability("appPackage", "br.com.lojong");
 		capabilities.setCapability("appActivity", "br.com.lojong.activity.SplashActivity");
 
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		log("--------------------------------------------------------", ILogLevel.TESTCASE);
+		log("---------Test ["+method.getName()+"] Started------------", ILogLevel.TESTCASE);
+		log("--------------------------------------------------------", ILogLevel.TESTCASE);
+		
+		
 	}
 
 	@AfterMethod
@@ -58,15 +60,10 @@ public class TestCore extends Page{
 			BaseClass.captureScreenshot();
 			BaseClass.takeScreenshot("Captured");
 		}
-
-	}
-
-	@AfterSuite
-
-	public void tearDown(){
+		
 		driver.quit();
-	}
 
+	}
 
 }
 
