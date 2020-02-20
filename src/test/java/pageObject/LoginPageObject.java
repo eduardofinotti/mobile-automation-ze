630841fbb252dd7fcf590ce2559b480f89eec230
gitsachin <qasachin03@gmail.com>
2020-02-20 18:04:45 +0530

    locators files

--- src/test/java/pageObject/LoginPageObject.java
+++ src/test/java/pageObject/LoginPageObject.java
@@ -1,6 +1,6 @@
 package pageObject;
 
-
+import org.openqa.selenium.By;
 
 public class LoginPageObject {
 	
@@ -9,15 +9,41 @@ public class LoginPageObject {
 	public static String name_id = "br.com.lojong:id/etName";
 	public static String email_id = "br.com.lojong:id/etEmail";
 	public static String password_id = "br.com.lojong:id/etPassword";
-	public static String submit_xpath = "//android.widget.Button[@index='6']";
-	public static String caminho_xpath = "//android.widget.RelativeLayout[@index='0']";
+	public static String submit_id = "br.com.lojong:id/btnCreateAccount";
+	public static String caminho_id = "br.com.lojong:id/llCaminho";
 	public static String one_id = "br.com.lojong:id/ivIconWay1";
 	public static String introduction_id = "br.com.lojong:id/tvTitleSession";
-	public static String closeButton_id = "br.com.lojong:id/ivRightIcon";
+	public static String closeButton_id = "br.com.lojong:id/ivLeftIcon";
+	public static String openButton_id = "br.com.lojong:id/ivRightIcon";
 	public static String yesButton_id = "android:id/button1";
 	public static String profiletab_id = "br.com.lojong:id/llProfile";
 	public static String profileName_id = "br.com.lojong:id/tvName";
-	public static String videoTab_id = "br.com.lojong:id/llVideos";
-	public static String videoPlaybutton_xpath = "//android.widget.ImageView[@index='1']";
+	public static String videoTab_id = "br.com.lojong:id/llInsight";
+	public static String videoPlaybutton_id = "br.com.lojong:id/llMain";
+	public static String depois_id = "br.com.lojong:id/tvAfeter";
+	public static String insight_id = "br.com.lojong:id/llInsight";
+	public static String artigos_id = "br.com.lojong:id/llBottomBar";
+	public static String citacoes_id = "br.com.lojong:id/btQuotes";
+	public static String timer_id = "br.com.lojong:id/llTimer";                            
+	public static String iniciar_id = "br.com.lojong:id/btStart";
+	public static String classname = "android.widget.TextView";
+	public static String programaCEB_id = "br.com.lojong:id/llProgramCeb";
+	public static String programaMind_id = "br.com.lojong:id/llProgramMindFullNess";
+	public static String equilibrioFirst_id = "br.com.lojong:id/tvProgramCeb";
+	public static String equilibrioSecond_id = "br.com.lojong:id/tvConativo";
+	public static String equilibrioThird_id = "br.com.lojong:id/tvCognitivo";
+	public static String equilibrioFourth_id = "br.com.lojong:id/tvAtencao";
+	public static String equilibrioFifth_id = "br.com.lojong:id/tvAfetivo";
+	public static String categoryText_id = "br.com.lojong:id/tvSelectedCategory";
+	public static String buttonBack_id = "br.com.lojong:id/llLeftIcon";
+	public static String topText_id = "br.com.lojong:id/tvTitleW8d7";
+	public static  String topElementOfScreen_id = "br.com.lojong:id/tvTitleW8d7";
+	public static String pagePROGRAMACEBHeader_id = "br.com.lojong:id/tvTitle";
+	public static String acolhendo_id= "br.com.lojong:id/llAnxiety";
+	public static String acolhendoNumbers_xpath= "//android.widget.TextView[@text='";
+	public static String acolhendoNumbers_xpath1 = "']";
+	public static String slideArrow_id= "br.com.lojong:id/ivSliderArrow";
+	public static String loginButton_xpath = "(//android.widget.Button[@index='0'])[2]";
+	public static String enterButton_xpath ="(//android.widget.Button[@index='0'])[1]";
 
 }
