fd283beb12b531c605a278b11ebf5d3032a942bb
Eduardo Finotti <eduardofinotti@ip-172-25-4-58.us-west-2.compute.internal>
2020-11-30 22:47:58 -0300

    improve project

--- src/test/java/hooks/BaseClass.java
+++ src/test/java/hooks/BaseClass.java
@@ -32,34 +32,6 @@ public class BaseClass {
 
     static AppiumDriverLocalService service;
 
-    public static void captureScreenshot() {
-        String name = new Date().toString();
-        String screenshotDirectory = System.getProperty("appium.screenshots.dir", System.getProperty("java.io.tmpdir", ""));
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        screenshot.renameTo(new File(screenshotDirectory, String.format("%s.png", name)));
-        log.info(screenshot.getPath());
-    }
-
-    public static void takeScreenshotAndSave(String screen) {
-        Capabilities cap = driver.get().getCapabilities();
-//        String device = (String) driver.get().getCapabilities().getCapability("deviceName");
-
-        String imagesLocation = "target/screenshots/" + device_run + '/';
-        new File(imagesLocation).mkdirs();
-        String filename = imagesLocation + screen + ".jpg";
-
-        try {
-            Thread.sleep(2000);
-            WebDriver augmentedDriver = new Augmenter().augment(driver.get());
-            File scrFile = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(scrFile, new File(filename), true);
-        } catch (Exception e) {
-            log.error("Error capturing screen shot of test failure.");
-            File f = new File(filename);
-            f.delete();
-        }
-    }
-
     public AppiumDriver getDriver() {
         return driver.get();
     }
@@ -68,7 +40,7 @@ public class BaseClass {
     @Parameters({"device", "osVersion", "local", "platform"})
     public void setup(String device, String osVersion, String local, String platform) {
 
-//        AppiumController.startAppium();
+        AppiumController.startAppium();
 
         local_run = local;
         device_run = device;
@@ -99,11 +71,38 @@ public class BaseClass {
     @AfterTest
     public void teardown() {
         driver.get().quit();
-//        service.stop();
+        service.stop();
 
         log.info("--------------------------------------------------------");
         log.info("----------------------Test Finish-----------------------");
         log.info("--------------------------------------------------------");
+    }
+
+    public static void captureScreenshot() {
+        String name = new Date().toString();
+        String screenshotDirectory = System.getProperty("appium.screenshots.dir", System.getProperty("java.io.tmpdir", ""));
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        screenshot.renameTo(new File(screenshotDirectory, String.format("%s.png", name)));
+        log.info(screenshot.getPath());
+    }
+
+    public static void takeScreenshotAndSave(String screen) {
+        Capabilities cap = driver.get().getCapabilities();
+
+        String imagesLocation = "target/screenshots/" + device_run + '/';
+        new File(imagesLocation).mkdirs();
+        String filename = imagesLocation + screen + ".jpg";
+
+        try {
+            Thread.sleep(2000);
+            WebDriver augmentedDriver = new Augmenter().augment(driver.get());
+            File scrFile = ((TakesScreenshot) augmentedDriver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File(filename), true);
+        } catch (Exception e) {
+            log.error("Error capturing screen shot of test failure.");
+            File f = new File(filename);
+            f.delete();
+        }
     }
 
 }
